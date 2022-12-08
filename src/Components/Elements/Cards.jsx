b0f2e9f3abb0db75db62e0b9da1d235811be06bf
ibne.abbassherazi89@gmail.com <ibne.abbassherazi89@gmail.com>
2022-12-08 17:55:18 +0500

    first commit

---
+++ src/Components/Elements/Cards.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Cards = () => {
+
+    
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-6">
+            <div className="card bg-light mb-3">
+              <div className="card-header text-center">Card1</div>
+              <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-6">
+            <div className="card bg-light mb-3">
+              <div className="card-header text-center">Card1</div>
+              <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-6">
+            <div className="card bg-light mb-3">
+              <div className="card-header text-center">Card1</div>
+              <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from '../Components/Elements/Cards'
+
+const Home = () => {
+  return (
+    <>
+      <Cards/>
+    </>
+  )
+}
+
+export default Home
